65826c8af18207c23d802b460d3f58e8264efd56
Yarik <yaroslav.matsapura@gmail.com>
2021-09-19 22:16:47 +0300

    pre-hook commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,9 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'airbnb-base',
-  ],
+  extends: ['airbnb-base'],
   parserOptions: {
     ecmaVersion: 12,
   },
-  rules: {
-  },
+  rules: {},
 };
